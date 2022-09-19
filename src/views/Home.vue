72333728ccfc56c358bb98b3b1217354dcb692e2
Antarip Saha <antarip15@gmail.com>
2022-09-19 12:46:30 +0600

    banner added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,14 @@
   <FeaturedCategories />
   <!-- <RecommendedServices /> -->
   <FeaturedProvider />
+  <SliderTwo />
   <NewestProvider />
 </template>
 
 <script>
 
 import Slider from '../components/home/slider.vue'
+import SliderTwo from '../components/home/slider_two.vue'
 import FeaturedCategories from '../components/home/featured_categories.vue'
 import RecommendedServices from '../components/home/recommended_services.vue'
 import NewestProvider from '../components/home/NewestProvider.vue'
@@ -17,6 +19,7 @@ import FeaturedProvider from '../components/home/FeaturedProvider.vue'
 export default {
   components: {
     Slider,
+    SliderTwo,
     FeaturedCategories,
     NewestProvider,
     FeaturedProvider,
