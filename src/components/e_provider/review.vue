631efd173b24c3ce1fe8061a00e43e4ab264655a
Antarip Saha <antarip15@gmail.com>
2022-09-27 11:55:02 +0600

    review updated

--- src/components/e_provider/review.vue
+++ src/components/e_provider/review.vue
@@ -10,7 +10,7 @@
                     <vue3-star-ratings v-model="posts.rate" inactiveColor="#D1D5D9" controlBg="#ffff" aria-required="true" />
                     <textarea name="review" v-model="posts.review" id="" cols="30" rows="5" class="block w-full rounded-md border-gray-300 shadow-sm focus:ring-main-color-500 focus:border-main-color-500 sm:text-sm" v-bind:aria-required="true"></textarea>
                     <button
-                        class='mt-3 flex justify-center px-4 py-2 text-sm font-medium text-white rounded-md border border-transparent shadow-sm bg-main-color-600 hover:bg-main-color-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-main-color-500' v-bind:aria-required="true" @click="errorMsg">
+                        class='mt-3 flex justify-center px-4 py-2 text-sm font-medium text-white rounded-md border border-transparent shadow-sm bg-main-color-600 hover:bg-main-color-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-main-color-500' v-bind:aria-required="true" @click="saveRate">
                         {{ $t('Save') }}
                     </button>
                 </div>
@@ -29,12 +29,11 @@
   </template>
  
   <script> 
-
   import axios from 'axios'
-
   import { createNamespacedHelpers } from 'vuex'
   import vue3StarRatings from "vue3-star-ratings";
   import { mapActions } from 'vuex'
+  import { async } from 'vuejs3-google-maps';
   const app_url = import.meta.env.VUE_APP_BASE_URL
 
   const { mapState, mapGetters } = createNamespacedHelpers('user')
@@ -57,28 +56,17 @@
     computed: {
       ...mapState(['user']),
       ...mapGetters(['getUser']),
-      
     },
-    
     methods: {
         ...mapActions('snackbar', ['toggleSnackBarAction']),
-
-    errorMsg(response){
-        this.saveRate();
-        console.log(response)
-        this.toggleSnackBarAction(response);
-    },
-    
      saveRate(){
-        
-        
         let response = ''
         this.posts.api_token = this.getUser.api_token,
         this.posts.user_id = this.getUser.id,
         this.posts.e_provider_id = this.$route.params.id,
-        response = axios.post('https://admin.yellowpageja.com/api/e_provider_reviews', this.posts).then(response => {
+         response =  axios.post('http://192.168.88.181:8000/api/e_service_reviews', this.posts).then(response => {
             console.log('response ', response )
-            if(response.data.success === 200){
+            if(response.data.success === true){
                 this.$notify({
                     type: 'success',
                     title: 'Data Saved',
@@ -86,14 +74,15 @@
                 })
                 setTimeout(() => {
                     window.location.reload();
-                }, 1000)
-            }else{
-                this.$notify({
-                    type: 'error',
-                    title: 'Data Error',
-                    message: 'Enter Data'
-                })
+                }, 500)
             }
+        }).catch(err => {
+            console.log("Error Messages::",err)
+            this.$notify({
+                    type: 'error',
+                    title: 'Error',
+                    message: "Rate Must Be Given"
+                })
         })
       },
     
